01bc850ab856074c3c60cf4a71b1a9c6697a628c
Zero <samuelbustos10903@gmail.com>
2023-05-22 15:47:06 -0300

    feat: home structure added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,32 @@
 <script setup></script>
 <template>
-	<div>
-		<h2>Pagina de inicio</h2>
-	</div>
+	<main class="home">
+		<h1 class="home__title">¡Bienvenido!</h1>
+		<p class="home__text">
+			En este pequeño rincón de internet, podrás encontrar diversas
+			recomendaciones de libros que me he leído durante este último tiempo. Las
+			mismas son totalmente subjetivas, pues dependen pura y exclusivamente de
+			mis gustos.
+		</p>
+		<p class="home__text">
+			Si tienes alguna recomendación que hacer, te invito a enviarmela por
+			correo
+		</p>
+	</main>
 </template>
+<style lang="scss" scoped>
+@use '../assets/sass/layout';
+@import '../assets/sass/settings';
+
+.home {
+	height: 80vh;
+	padding: 1em;
+	text-align: center;
+	@include layout.flex(column, nowrap, center, center);
+
+	&__title {
+		padding-bottom: 1em;
+		font-size: $title-1;
+	}
+}
+</style>
