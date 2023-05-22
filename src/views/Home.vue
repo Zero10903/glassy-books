69505a44c8ea3809dd8d01d706f8da2999b061c4
Zero <samuelbustos10903@gmail.com>
2023-05-22 17:25:25 -0300

    feat: Home section added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,24 @@
 <script setup></script>
 <template>
 	<main class="home">
-		<h1 class="home__title">¡Bienvenido!</h1>
-		<p class="home__text">
-			En este pequeño rincón de internet, podrás encontrar diversas
-			recomendaciones de libros que me he leído durante este último tiempo. Las
-			mismas son totalmente subjetivas, pues dependen pura y exclusivamente de
-			mis gustos.
-		</p>
-		<p class="home__text">
-			Si tienes alguna recomendación que hacer, te invito a enviarmela por
-			correo
-		</p>
+		<img src="../assets/images/logo.png" alt="logo" class="home__img" />
+		<section class="home__content">
+			<h1 class="home__title">¡Bienvenido a Glassy Books!</h1>
+			<p class="home__text">
+				En este pequeño rincón de internet, podrás encontrar diversas
+				recomendaciones de libros que me he leído durante este último tiempo.
+				Las mismas son totalmente subjetivas, pues dependen pura y
+				exclusivamente de mis gustos.
+			</p>
+			<p class="home__text">
+				Si tienes alguna recomendación que hacer, te invito a enviarmela por
+				correo
+			</p>
+		</section>
 	</main>
 </template>
 <style lang="scss" scoped>
+@use '../assets/sass/text';
 @use '../assets/sass/layout';
 @import '../assets/sass/settings';
 
@@ -27,6 +31,9 @@
 	&__title {
 		padding-bottom: 1em;
 		font-size: $title-1;
+		font-weight: bold;
+
+		@include text.gradient($color-light, $color-dark);
 	}
 }
 </style>
