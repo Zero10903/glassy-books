da6301f108cdc90f3f41a97f28515c0035bf3457
Zero <samuelbustos10903@gmail.com>
2023-06-02 00:13:10 -0300

    feat: new book added

--- src/data/books.js
+++ src/data/books.js
@@ -1,6 +1,6 @@
 export const bookData = [
 	{
-		title: 'La Vida Invisible de Addie LaRue',
+		title: 'La vida invisible de Addie LaRue',
 		url: 'https://images.cdn3.buscalibre.com/fit-in/360x360/ed/a9/eda9a2b37115bc13b8a92bcaa3e97015.jpg',
 		description: `Este libro es una obra maestra creada por V.E. Schwab, una escritora muy famosa conocida por obras como 
         'Gallant' o la saga 'Shades of Magic'. La historia, en este caso, nos cuenta la vida de una chica llamada Adeline quien, 
@@ -12,5 +12,21 @@ export const bookData = [
         un momento muy inesperado conoce a alguien que, sin ella saber el porqué, puede recordarla. 
         Este es uno de esos libros que te marcan de por vida, tiene muchos momentos hermosos y frases preciosas que te 
         darán mucho en lo que pensar. Te lo recomiendo encarecidamente ^^`,
+	},
+	{
+		title: 'La ciudad de los fantasmas',
+		url: 'https://www.polifemo.com/static/img/portadas/_visd_0000JPG015HF.jpg',
+		description: `Este libro tambien fue creado por V.E. Schwab. La historia nos cuenta la vida de nuestra protagonista: Cassidy Blake, 
+        la cual es normal... hasta cierto punto. Digamos que Cass tiene una 'habilidad especial': es capaz ver fantasmas atravesando un 
+        velo que sólo ella puede ver. Al atravesar el mismo, entra a una especie de mundo alterno que es igual al lugar en el que estaba, pero en 
+        una determinada fecha y hora del pasado. Allí es donde encuentra a los fantasmas. A través del velo puede ver la historia de cómo 
+        murieron una o varias personas. Además, Cassiy no va sola. Sino que está acompañada por Jacob, su compañero de aventuras que, por cierto, 
+        tambien es un fantasma; y es el único capaz de atravesar el velo con ella. Durante todo el libro y la trilogía como tal, estaremos 
+        viendo las aventuras de Cassidy a través del velo y en el mundo real, alternando entre uno y otro para desentrañar los secretos 
+        que se ocultan en el mismo. Buscando aventuras e historias increibles, pero tambien investigando por qué ella es capaz de 
+        atravesar el velo, y por qué Jacob tambien puede hacerlo.
+        Te lo recomiendo muchísimo si te gustan las historias de terror y drama con componentes de alta tensión y, si no te gustan 
+        las historias de este tipo, te diría que igualmente le des una oportunidad. Yo no suelo leer cosas de este género y 
+        aun así me enamoré de la historia.`,
 	},
 ];
